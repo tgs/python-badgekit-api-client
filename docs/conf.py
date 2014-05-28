c8ae38e59bb0c15a48638307d5d522a76ad1ba37
Thomas Grenfell Smith <thomathom@gmail.com>
2014-05-28 14:29:47 -0400

    Fix doc generation now that jwt_auth is gone

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['jwt']
+MOCK_MODULES = ['requests_jwt']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
